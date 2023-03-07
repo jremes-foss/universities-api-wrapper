d9c79a2a93572e0f9c63cb5b08f383ba26d00ded
Juha Remes <jremes@outlook.com>
2023-03-07 00:11:37 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Universities API Wrapper'
+LONG_DESCRIPTION = 'Python package for consuming universities-domains-list API'
+
+# Setting up
+setup(
+        name="universities-api-wrapper",
+        version=VERSION,
+        author="Juha Remes",
+        author_email="jremes@outlook.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['requests', 'mock'],
+        keywords=['python', 'api', 'wrapper', 'education', 'universities'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
