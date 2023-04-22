aeb9c7784106b0bda22a5976499f47e1a1eba2d2
Juha Remes <jremes@outlook.com>
2023-04-22 23:37:50 +0100

    setup.py: bump 0.0.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Universities API Wrapper'
 LONG_DESCRIPTION = 'Python package for consuming universities-domains-list API'
 
